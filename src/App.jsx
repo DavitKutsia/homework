9e747fcbf26f9cd62978659c269a33236692ad4d
unknown <itzarctic33@gmail.com>
2024-10-04 22:36:04 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <div>
+
+      <h1 className='Text '>The Product</h1>
+
+      <div className='product-card'>
+        <h2>Teleporter</h2>
+        <p>It can Teleport you anywhere.</p>
+        <p>Price: only $69.99</p>
+        <p>Its almost sold out and you will never see such a discount EVER, so BUY IT NOW!!!</p>        
+    </div>
+
+    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+    <button>BUY IT NOW</button>
+    </a>
+
+    </div>
+
+  )
+}
+
+export default App
